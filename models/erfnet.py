4448d44150d8356dc998809dc8ea7ad7bc1ea3f9
jpbacher <bacherjp@gmail.com>
2020-04-21 20:28:41 -0400

    completed implementation

--- models/erfnet.py
+++ models/erfnet.py
@@ -1,18 +1,17 @@
 import torch
 import torch.nn as nn
-import torch.nn.init as init
 import torch.nn.functional as F
 
 
-class DownSampleBlock(nn.Module):
-    def __init__(self, input, output):
+class DownSample(nn.Module):
+    def __init__(self, inp, out):
         super().__init__()
-        self.conv = nn.Conv2d(input, output - input, (3, 3), stride=2, padding=1, bias=True)
+        self.conv = nn.Conv2d(inp, out - inp, (3, 3), stride=2, padding=1, bias=True)
         self.pool = nn.MaxPool2d(2, stride=2)
-        self.bn = nn.BatchNorm2d(output, eps=1e-3)
+        self.bn = nn.BatchNorm2d(out, eps=1e-3)
 
-    def forward(self, input):
-        out = torch.cat([self.conv(input), self.pool(input)], dim=1)
+    def forward(self, inp):
+        out = torch.cat([self.conv(inp), self.pool(inp)], dim=1)
         out = self.bn(out)
         return F.relu(out)
 
@@ -28,8 +27,8 @@ class NonBottleneck1d(nn.Module):
         self.bn2 = nn.BatchNorm2d(channel, eps=1e-3)
         self.dropout = nn.Dropout2d(drop_prob)
 
-    def forward(self, input):
-        out = self.conv3x1_1(input)
+    def forward(self, inp):
+        out = self.conv3x1_1(inp)
         out = F.relu(out)
         out = self.conv1x3_1(out)
         out = self.bn1(out)
@@ -41,6 +40,78 @@ class NonBottleneck1d(nn.Module):
         out = self.bn2(out)
         if self.dropout != 0:
             out = self.dropout(out)
-        return F.relu(out+input)  # residual connection (identity)
+        return F.relu(out+inp)  # residual connection (identity)
 
 
+class Encoder(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.initial_block = DownSample(3, 16)
+        self.layers = nn.ModuleList()
+        self.layers.append(DownSample(16, 64))
+        for l in range(5):
+            self.layers.append(NonBottleneck1d(64, 0.03, 1))
+        self.layers.append(DownSample(64, 128))
+        for l in range(2):
+            self.layers.append(NonBottleneck1d(128, 0.3, 2))
+            self.layers.append(NonBottleneck1d(128, 0.3, 4))
+            self.layers.append(NonBottleneck1d(128, 0.3, 8))
+            self.layers.append(NonBottleneck1d(128, 0.3, 16))
+        self.out_conv = nn.Conv2d(128, num_classes, 1, stride=1, padding=0, bias=True)
+
+    def forward(self, inp, predict=False):
+        out = self.initial_block(inp)
+        for layer in self.layers:
+            out = layer(out)
+        if predict:
+            out = self.out_conv(out)
+        return out
+
+
+class UpSample(nn.Module):
+    def __init__(self, inp, out):
+        super().__init__()
+        self.conv_t = nn.ConvTranspose2d(inp, out, 3, stride=2, padding=1, output_padding=1, bias=True)
+        self.bn = nn.BatchNorm2d(out, eps=1e-3)
+
+    def forward(self, inp):
+        out = self.conv_t(inp)
+        out = self.bn(out)
+        return F.relu(out)
+
+
+class Decoder(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        self.layers.append(UpSample(128, 64))
+        self.layers.append(NonBottleneck1d(64, 0, 1))
+        self.layers.append(NonBottleneck1d(64, 0, 1))
+        self.layers.append(UpSample(64, 16))
+        self.layers.append(NonBottleneck1d(16, 0, 1))
+        self.layers.append(NonBottleneck1d(16, 0, 1))
+        self.out_conv_t = nn.ConvTranspose2d(16, num_classes, 2, stride=2, padding=0, output_padding=0, bias=True)
+
+    def forward(self, inp):
+        out = inp
+        for layer in self.layers:
+            out = layer(out)
+        out = self.out_conv_t(out)
+        return out
+
+
+class Net(nn.Module):
+    def __init__(self, num_classes, encoder=None):
+        super().__init__()
+        if encoder is None:
+            self.encoder = Encoder(num_classes)
+        else:
+            self.encoder = encoder
+        self.decoder = Decoder(num_classes)
+
+    def forward(self, inp, encode_only=False):
+        if encode_only:
+            return self.encoder.forward(inp, predict=True)
+        else:
+            out = self.encoder(inp)
+            return self.decoder.forward(out)
