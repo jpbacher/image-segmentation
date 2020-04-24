38480af5dba6d597335bfb89ccdc423a287d50d5
jpbacher <bacherjp@gmail.com>
2020-04-24 17:19:39 -0400

    changed loss function

--- losses/__init__.py
+++ losses/__init__.py
@@ -6,11 +6,11 @@ def dice_coeff(y_true, y_pred, smooth=1.0):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersect = K.sum(y_true_f * y_pred_f)
-    d_coeff = (2.0 * intersect + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-    return d_coeff
+    dice = (2.0 * intersect + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return dice
 
 
 def dice_loss_bce(y_true, y_pred):
     bce = BinaryCrossentropy()
-    dice_bce = 0.5 * bce(y_true, y_pred) - dice_coeff(y_true, y_pred)
+    dice_bce = 1 + bce(y_true, y_pred) - dice_coeff(y_true, y_pred)
     return dice_bce
