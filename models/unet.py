7039ee9eda59652ae0579dd01464606fc1d106d4
jpbacher <bacherjp@gmail.com>
2020-04-25 14:29:47 -0400

    adding BN to after Conv layers

--- models/unet.py
+++ models/unet.py
@@ -2,36 +2,47 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, BatchNormalization, Conv2D, MaxPooling2D, Conv2DTranspose, concatenate
 
 
-def Unet(input_size=(256, 256, 3), batch_size=None, pretrained_weights=None):
+def unet(input_size=(256, 256, 3), batch_size=None, pretrained_weights=None):
 
     inputs = Input(shape=input_size, batch_size=batch_size)
-    bnorm1 = BatchNormalization()(inputs)
-    conv1 = Conv2D(32, (3, 3), padding='same', activation='relu')(bnorm1)
+    conv1 = Conv2D(32, (3, 3), padding='same', activation='relu')(inputs)
+    bnorm1 = BatchNormalization()(conv1)
     conv1 = Conv2D(32, (3, 3), padding='same', activation='relu')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    bnorm1 = BatchNormalization()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(bnorm1)
 
     conv2 = Conv2D(64, (3, 3), padding='same', activation='relu')(pool1)
+    bnorm2 = BatchNormalization()(conv2)
     conv2 = Conv2D(64, (3, 3), padding='same', activation='relu')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    bnorm2 = BatchNormalization()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(bnorm2)
 
     conv3 = Conv2D(128, (3, 3), padding='same', activation='relu')(pool2)
-    conv3 = Conv2D(128, (3, 3), padding='same', activation='relu')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    bnorm3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(128, (3, 3), padding='same', activation='relu')(bnorm3)
+    bnorm3 = BatchNormalization()(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(bnorm3)
 
     conv4 = Conv2D(256, (3, 3), padding='same', activation='relu')(pool3)
-    conv4 = Conv2D(256, (3, 3), padding='same', activation='relu')(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    bnorm4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(256, (3, 3), padding='same', activation='relu')(bnorm4)
+    bnorm4 = BatchNormalization()(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(bnorm4)
 
     conv5 = Conv2D(512, (3, 3), padding='same', activation='relu')(pool4)
+    bnorm5 = BatchNormalization()(conv5)
     conv5 = Conv2D(512, (3, 3), padding='same', activation='relu')(conv5)
+    bnorm5 = BatchNormalization()(conv5)
 
-    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5),
-                      conv4], axis=3)
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(bnorm5),
+                      bnorm4], axis=3)
     conv6 = Conv2D(256, (3, 3), padding='same', activation='relu')(up6)
-    conv6 = Conv2D(256, (3, 3), padding='same', activation='relu')(conv6)
+    bnorm6 = BatchNormalization()(conv6)
+    conv6 = Conv2D(256, (3, 3), padding='same', activation='relu')(bnorm6)
+    bnorm6 = BatchNormalization()(conv6)
 
-    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6),
-                       conv3], axis=3)
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(bnorm6),
+                       bnorm3], axis=3)
     conv7 = Conv2D(128, (3, 3), padding='same', activation='relu')(up7)
     conv7 = Conv2D(128, (3, 3), padding='same', activation='relu')(conv7)
 
